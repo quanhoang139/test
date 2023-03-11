82e0f1103659532ffb3fdea72dd7ad18a06861a2
Hoàng Mạnh Quân <100327053+quanhoang139@users.noreply.github.com>
2023-03-11 22:35:34 +0700

    Update c.c

--- c.c
+++ c.c
@@ -1,45 +1,38 @@
-#include <stdio.h>
+#include <unistd.h>
+#include <sys/types.h>
+#include <sys/wait.h>
+#include <iostream>
 
-int main() {
-    int i, j, n, time_quantum, time, remain, flag = 0;
-    int sum_wait = 0, sum_turnaround = 0, arrival_time[100], burst_time[100], rt[100];
-    printf("Enter the number of Processes: ");
-    scanf("%d", & n);
-    remain = n;
-    for (i = 0; i < n; i++) {
-        printf("Enter arrival_time and burst_time for Process P%d: ", i + 1);
-        scanf("%d", & arrival_time[i]);
-        scanf("%d", & burst_time[i]);
-        rt[i] = burst_time[i];
-    }
-    printf("Enter time quantum: ");
-    scanf("%d", & time_quantum);
-    printf("\nProcess\t|Turnaround time|Waiting time\n");
-    printf("--------+---------------+------------\n");
-    for (time = 0, i = 0; remain != 0;) {
-        if (rt[i] <= time_quantum && rt[i] > 0) {
-            time += rt[i];
-            rt[i] = 0;
-            flag = 1;
-        } else if (rt[i] > 0) {
-            rt[i] -= time_quantum;
-            time += time_quantum;
-        }
-        if (rt[i] == 0 && flag == 1) {
-            remain--;
-            printf("P[%d]\t|\t%d\t|\t%d\n", i + 1, time - arrival_time[i], time - arrival_time[i] - burst_time[i]);
-            sum_wait += time - arrival_time[i] - burst_time[i];
-            sum_turnaround += time - arrival_time[i];
-            flag = 0;
-        }
-        if (i == n - 1)
-            i = 0;
-        else if (arrival_time[i + 1] <= time)
-            i++;
-        else
-            i = 0;
-    }
-    printf("\nAverage Waiting Time = %f\n", sum_wait * 1.0 / n);
-    printf("Average Turnaround Time = %f", sum_turnaround * 1.0 / n);
-    return 0;
+
+using namespace std;
+
+
+int main()
+{
+   pid_t pid = fork(); // tạo một tiến trình con
+
+
+   if (pid == 0)
+   {
+       // tiến trình con
+       std::cout << "Child process is running" << std::endl;
+       sleep(10); // Child process becomes orphan after 10 seconds
+       std::cout << "Child process is now orphan" << std::endl;
+   }
+   else if (pid > 0)
+   {
+       // tiến trình cha
+       std::cout << "Parent process is running" << std::endl;
+       wait(NULL); // Wait for child process to finish
+   }
+   else
+   {
+       std::cerr << "Fork failed" << std::endl;
+       return 1;
+   }
+
+
+   // khi tiến trình cha kết thúc, tiến trình con trở thành Orphan
+   cout << "Tiến trình kết thúc." << endl;
+   return 0;
 }
